643723bda3212baf130bf0d736463ce3db17fe6c
Ash Powell <ash-powell@hotmail.co.uk>
2022-04-06 12:40:28 +0100

    Update Balance.php

--- src/Api/Request/Accounts/Balance.php
+++ src/Api/Request/Accounts/Balance.php
@@ -4,6 +4,7 @@ namespace Starling\Api\Request\Accounts;
 
 use Starling\Api as Base;
 use Starling\Api\Request;
+use Starling\Exception\RequiredValuesMissing;
 
 class Balance extends Request
 {
@@ -12,7 +13,7 @@ class Balance extends Request
      *
      * @var string
      */
-    protected $endpoint = 'accounts/balance';
+    protected $endpoint = 'accounts/{accountId}/balance';
 
     /**
      * Whats our endpoint.
@@ -20,4 +21,39 @@ class Balance extends Request
      * @var string
      */
     protected $type = Base::TYPE_GET;
+
+    /**
+     * Hold our Account ID.
+     *
+     * @var string
+     */
+    protected $account_id;
+
+    /**
+     * Build our request.
+     *
+     * @param array $values
+     *
+     * @return void
+     */
+    public function __construct($values = [])
+    {
+        if (!isset($values['account_id'])) {
+            throw new RequiredValuesMissing(['account_id']);
+        }
+
+        $this->account_id = $values['account_id'];
+    }
+
+    /**
+     * Get endpoint.
+     *
+     * @return string
+     */
+    public function getEndpoint()
+    {
+        return strtr($this->endpoint, [
+            '{accountId}'  => $this->account_id,
+        ]);
+    }
 }
